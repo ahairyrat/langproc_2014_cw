5adc32147a599f47ed757ea14d7e1bb2c4766e71
ahairyrat <patengelbert@hotmail.co.uk>
2015-03-01 12:00:45 +0000

    Made framework for config

--- c_compiler/includes/Config.h
+++ c_compiler/includes/Config.h
@@ -5,42 +5,22 @@
 #include <map>
 #include <string>
 
-class Config{
+//////////////////////////////////////////////////
+//List of all global config modifiers
+extern bool DISPLAYAST;
 
-public:
-	//Add themselves to the map of config functions
-	Config();
 
-	virtual void configure(std::string);
 
-	//remove themselves from the map of config functions
-	~Config();
 
-private:
-	std::string name;
 
-};
+/////////////////////////////////////////////////
 
-typedef void(Config::*configFunctionPtr)(std::string);
+typedef void(*configFunctionPtr)(std::string);
 
 //Global map of all configuration functions
 //Functions can add themselves to this at runtime
-std::map<std::string, configFunctionPtr> configFunctions;
+extern std::map<std::string, configFunctionPtr> configFunctions;
 
-
-//Add themselves to the map of config functions
-Config::Config(){
-	configFunctions[name] = &Config::configure;
-}
-
-//remove themselves from the map of config functions
-Config::~Config(){
-	configFunctions.erase(name);
-}
-
-void Config::configure(std::string){
-
-}
-
+void initialiseConfigs();
 
 #endif
--- c_compiler/src/compiler.cpp
+++ c_compiler/src/compiler.cpp
@@ -7,18 +7,45 @@
 #include "Translator.h"
 
 #include <fstream>
+#include <iostream>
 #include <string>
 
 using namespace std;
+
+void loadConfigurations(ifstream &configFile);
 
 int main()
 {
 	if (USECONGFIGFILE)
 	{
+		initialiseConfigs();
 		ifstream configFile;
-		configFile.open("compiler.ini");
-		//loadConfigurations(configFile, configFunctions);
+		configFile.open("config.ini");
+		if (!configFile.is_open())
+		{
+			cout << "No configuration file found\nUsing defaults" << endl;
+		}
+		else
+			loadConfigurations(configFile);
+
 		configFile.close();
 	}
+
+	getchar();
 	return 0;
+}
+
+void loadConfigurations(ifstream &configFile)
+{
+	std::string line, cmd, value;
+	int cmdEnd;
+	while (getline(configFile, line))
+	{
+		cmdEnd = line.find(' ');
+		if (cmdEnd < 0)
+			break;
+		cmd = line.substr(0, cmdEnd);
+		value = line.substr(cmdEnd);
+		configFunctions[cmd](value);
+	}
 }
